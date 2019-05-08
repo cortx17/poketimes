097a55a67671f961ee2ea67b2941ff6cfa3a5e49
cortx17 <pulsar.zero@gmail.com>
2019-05-08 14:27:23 +0200

    Updates

---
+++ precache-manifest.c79047816b76d1263e2ffb855acebbbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/poketimes/static/media/pokeball.f10bf078.png"
+  },
+  {
+    "revision": "f792397527fa136ab836",
+    "url": "/poketimes/static/js/runtime~main.1a3268b9.js"
+  },
+  {
+    "revision": "90e3f30784a87aca5632",
+    "url": "/poketimes/static/js/main.c4272db0.chunk.js"
+  },
+  {
+    "revision": "b2d0b9e811a965b8502e",
+    "url": "/poketimes/static/js/2.b7325fa2.chunk.js"
+  },
+  {
+    "revision": "90e3f30784a87aca5632",
+    "url": "/poketimes/static/css/main.bc6b0b3a.chunk.css"
+  },
+  {
+    "revision": "f1dc88daf014dfdd8cc92750fadd8a04",
+    "url": "/poketimes/index.html"
+  }
+];
